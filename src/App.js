b3c55684150d09ee594782f365921764fb369024
KU Lok Sun <sundaykuloksun@gmail.com>
2019-09-16 13:56:19 +0800

    chore: clean up deprecated code and style

--- src/App.js
+++ src/App.js
@@ -1,5 +1,4 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
 
 import PageContainer from './PageContainer'
@@ -7,20 +6,6 @@ import PageContainer from './PageContainer'
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
       <PageContainer/>
     </div>
   );
