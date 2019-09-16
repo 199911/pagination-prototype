30ae61a32bf2f2fc42de339228fc6797dfe9447d
KU Lok Sun <sundaykuloksun@gmail.com>
2019-09-16 17:55:11 +0800

    fix: update fields name in frontend

--- src/Card.js
+++ src/Card.js
@@ -1,12 +1,12 @@
 import React from 'react';
 
 const Card = (props) => {
-    const { name, like, createdAt } = props
-    const createAtStr = (new Date(createdAt)).toDateString()
+    const { name, likes, created_at } = props
+    const createAtStr = (new Date(created_at)).toDateString()
     return (
         <div className='Card'>
             <h3>{ name }</h3>
-            <p>{ `Likes: ${like}` }</p>
+            <p>{ `Likes: ${likes}` }</p>
             <p>{ `Created At: ${createAtStr}` }</p>
         </div>
     )
