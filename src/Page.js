fed67062d1734222b2351d00e87bce13f2a8d532
KU Lok Sun <sundaykuloksun@gmail.com>
2019-09-16 14:42:36 +0800

    feat: add page style

--- src/Page.js
+++ src/Page.js
@@ -4,8 +4,10 @@ import Card from './Card'
 const Page = (props) => {
     const {items, reload, getNextPage} = props
     return (
-        <div>
-            { items.map((item, index) => <Card key={index} {...item} />) }
+        <div className='Page'>
+            <div className='CardContainer'>
+                { items.map((item, index) => <Card key={index} {...item} />) }
+            </div>
             <button onClick={reload}>Reload</button>
             <button onClick={getNextPage}>Next page</button>
         </div>
