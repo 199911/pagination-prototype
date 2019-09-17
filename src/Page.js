be6d1409c8161f3ea9da8e9fd933b7dd06deb253
KU Lok Sun <sundaykuloksun@gmail.com>
2019-09-17 15:11:34 +0800

    feat: only show first 3 item in page

--- src/Page.js
+++ src/Page.js
@@ -7,8 +7,9 @@ const Page = (props) => {
         <div className='Page'>
             <h1>{ `current: ${currentPage} nextPage: ${nextPage}`}</h1>
             <div className='CardContainer'>
-                { items.map((item, index) => <Card key={index} {...item} />) }
+                { items.slice(0,3).map((item, index) => <Card key={index} {...item} />) }
             </div>
+            { items.length > 3 && `And ${items.length - 3} more items`}
             <button onClick={reload}>Reload</button>
             { getNextPage && <button onClick={getNextPage}>Next page</button> }
         </div>
